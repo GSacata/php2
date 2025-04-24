cc7a295a5b1fe52500394e142a55574e78c747de
GSacata <gsacata@gmail.com>
2025-04-24 10:32:59 -0300

    Small fixes to pages

---
+++ validador-cpf/validador-cpf.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validador de CPF</title>
+</head>
+<body>
+    <div>
+        <?php echo "Chamou validador-cpf.php" ?>
+    </div>
+</body>
+</html>
