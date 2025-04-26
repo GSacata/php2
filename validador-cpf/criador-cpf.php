34529abbc4b2ca0367940c0d4c7a875cf1f38e85
GSacata <gsacata@gmail.com>
2025-04-26 17:02:43 -0300

    Created CPF creator.

--- validador-cpf/criador-cpf.php
+++ validador-cpf/criador-cpf.php
@@ -1,13 +1,19 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Validador de CPF</title>
-</head>
-<body>
-    <div>
-        <?php echo "Chamou criador-cpf.php" ?>
-    </div>
-</body>
-</html>
+<?php
+    namespace CPFCreator;
+    
+    require_once "validador-cpf.php";
+    use function CPFValidator\calculateCPFLastDigits;
+
+    $arrayCPFIncomplete = [];
+
+    for ($i = 0; $i < 9; $i += 1) {
+        $randNum = rand(0, 9);
+        array_push($arrayCPFIncomplete, $randNum);
+    };
+
+    $arrayLastDigits = calculateCPFLastDigits($arrayCPFIncomplete);
+    $arrayCPFComplete = array_merge($arrayCPFIncomplete, $arrayLastDigits);
+
+    $CPFString = implode($arrayCPFComplete);
+    echo "$CPFString";
+?>
--- validador-cpf/validador-cpf.php
+++ validador-cpf/validador-cpf.php
@@ -1,109 +1,96 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Validador de CPF</title>
-</head>
-<body>
-    <div>
-        <?php
-            echo "Chamou validador-cpf.php\n";
+<?php
+    namespace CPFValidator;
+    
+    $requestCPF = $_POST["cpf-validator-field-cpf-to-validate"]; // Vem como string
+    // $requestCPF = "367.522.200-42"; // DEV-TESTE
+    // $requestCPF = "367.522.200-1"; // DEV-TESTE
+    // $requestCPF = "367.522.200-1a"; // DEV-TESTE
+    // $requestCPF = "367.522.264-42"; // DEV-TESTE
+    // $requestCPF = "22222222222"; // DEV-TESTE
+    // $requestCPF = "22222222221"; // DEV-TESTE
+    // $requestCPF = "02926218761";
 
-            $requestCPF = $_POST["cpf-validator-field-cpf-to-validate"]; // Vem como string
-            // $requestCPF = "367.522.200-42"; // DEV-TESTE
-            // $requestCPF = "367.522.200-1"; // DEV-TESTE
-            // $requestCPF = "367.522.200-1a"; // DEV-TESTE
-            // $requestCPF = "367.522.264-42"; // DEV-TESTE
-            // $requestCPF = "22222222222"; // DEV-TESTE
-            // $requestCPF = "22222222221"; // DEV-TESTE
+    function validateCPF($postedCPF) {
+        $postedCPFClean = preg_replace("/\D/", "", $postedCPF);
+        
+        // verif de tamanho
+        if (strlen($postedCPFClean) != 11) return false;
 
-            function validateCPF($postedCPF) {
-                $postedCPFClean = preg_replace("/\D/", "", $postedCPF);
-                
-                // verif de tamanho
-                if (strlen($postedCPFClean) != 11) return false;
+        // verif dígitso iguais
+        $postedCPFCleanArray = str_split($postedCPFClean);
+        $arrayCPFSingleNumber = array_unique($postedCPFCleanArray);
+        if (count($arrayCPFSingleNumber) == 1) {
+            return false;
+        }
 
-                // verif dígitso iguais
-                $postedCPFCleanArray = str_split($postedCPFClean);
-                $arrayCPFSingleNumber = array_unique($postedCPFCleanArray);
-                if (count($arrayCPFSingleNumber) == 1) {
-                    return false;
-                }
+        $CPFIncomplete = substr($postedCPFClean, 0, 9);
+        $arrayCPFIncomplete = str_split($CPFIncomplete);
 
-                $CPFIncomplete = substr($postedCPFClean, 0, 9);
-                $arrayCPFIncomplete = str_split($CPFIncomplete);
+        $arrayCPFIncomplete = array_map(function ($x) {
+            return (int)$x;
+        }, $arrayCPFIncomplete);
+        
+        $arrayLastDigits = calculateCPFLastDigits($arrayCPFIncomplete);
+        $arrayCPFComplete = array_merge($arrayCPFIncomplete, $arrayLastDigits);
 
-                $arrayCPFIncomplete = array_map(function ($x) {
-                    return (int)$x;
-                }, $arrayCPFIncomplete);
-                
-                $arrayLastDigits = calculateCPFLastDigits($arrayCPFIncomplete);
-                $arrayCPFComplete = array_merge($arrayCPFIncomplete, $arrayLastDigits);
+        // remontando CPF
+        $refCPF = implode("", $arrayCPFComplete);
 
-                // remontando CPF
-                $refCPF = implode("", $arrayCPFComplete);
+        // comparando
+        return ($refCPF == $postedCPFClean) ? true : false;
+    };
 
-                // comparando
-                return ($refCPF == $postedCPFClean) ? true : false;
-            };
+    function calculateCPFLastDigits($arrayIncompleteCPF) {
+        $arrayLastDigits = [];
+        $totalFirstPart = 0;
+        
+        // calculando primeiro dígito
+        $paramFirstDigit = 10;
+        foreach ($arrayIncompleteCPF as $num) {
+            $totalFirstPart += ($num * $paramFirstDigit);
+            $paramFirstDigit -= 1;
+        }
+        
+        $divRestFirstDigit = ($totalFirstPart % 11);
+        $diffFirstDigit = 11 - $divRestFirstDigit;
+        
+        $firstDigit = 0;
+        if ($diffFirstDigit >= 10) {
+            $firstDigit = 0;
+        } else {
+            $firstDigit = $diffFirstDigit;
+        }
+        array_push($arrayLastDigits, $firstDigit);
+        array_push($arrayIncompleteCPF, $firstDigit);
 
-            function calculateCPFLastDigits($arrayIncompleteCPF) {
-                $arrayLastDigits = [];
-                $totalFirstPart = 0;
-                
-                // calculando primeiro dígito
-                $paramFirstDigit = 10;
-                foreach ($arrayIncompleteCPF as $num) {
-                    $totalFirstPart += ($num * $paramFirstDigit);
-                    $paramFirstDigit -= 1;
-                }
-                
-                $divRestFirstDigit = ($totalFirstPart % 11);
-                $diffFirstDigit = 11 - $divRestFirstDigit;
-                
-                $firstDigit = 0;
-                if ($diffFirstDigit >= 10) {
-                    $firstDigit = 0;
-                } else {
-                    $firstDigit = $diffFirstDigit;
-                }
-                array_push($arrayLastDigits, $firstDigit);
-                array_push($arrayIncompleteCPF, $firstDigit);
+        // calculadndo segundo dígito
+        $totalSecondPart = 0;
+        $paramSecondDigit = 11;
+        
+        foreach ($arrayIncompleteCPF as $num) {
+            $totalSecondPart += ($num * $paramSecondDigit);
+            $paramSecondDigit -= 1;
+        }
+        
+        $divRestSecondDigit = ($totalSecondPart % 11);
+        $diffSecondDigit = 11 - $divRestSecondDigit;
+        
+        $secondDigit = 0;
+        if ($diffSecondDigit >= 10) {
+            $secondDigit = 0;
+        } else {
+            $secondDigit = $diffSecondDigit;
+        }
+        array_push($arrayLastDigits, $secondDigit);
+        return $arrayLastDigits;
+    }
 
-                // calculadndo segundo dígito
-                $totalSecondPart = 0;
-                $paramSecondDigit = 11;
-                
-                foreach ($arrayIncompleteCPF as $num) {
-                    $totalSecondPart += ($num * $paramSecondDigit);
-                    $paramSecondDigit -= 1;
-                }
-                
-                $divRestSecondDigit = ($totalSecondPart % 11);
-                $diffSecondDigit = 11 - $divRestSecondDigit;
-                
-                $secondDigit = 0;
-                if ($diffSecondDigit >= 10) {
-                    $secondDigit = 0;
-                } else {
-                    $secondDigit = $diffSecondDigit;
-                }
-                array_push($arrayLastDigits, $secondDigit);
-                var_dump($arrayLastDigits);
-                return $arrayLastDigits;
-            }
+    $CPFIsValid = validateCPF($requestCPF);
+    
+    if ($CPFIsValid) {
+        echo "CPF válido.\n";
+    } else {
+        echo "CPF inválido.\n";
+    };
 
-            $CPFIsValid = validateCPF($requestCPF);
-            
-            if ($CPFIsValid) {
-                echo "CPF válido.\n";
-            } else {
-                echo "CPF inválido.\n";
-            };
-
-        ?>
-
-    </div>
-</body>
-</html>
+?>
